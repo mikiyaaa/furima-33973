077ba4f1bf763bd97cbfd95fa11929dc092b80a2
mikiyaaa <katocha.m0220thankyou@gmail.com>
2021-02-10 15:59:16 +0900

    ActiveHashを利用しプルダウンを表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,11 +4,20 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :item_name
     validates :description
-    validates :category_id
-    validates :status_id
-    validates :shipping_cost_id
-    validates :prefecture_id
-    validates :shipping_day_id
+    validates :category_id, numericality: { other_than: 1 }
+    validates :status_id, numericality: { other_than: 1 }
+    validates :shipping_cost_id, numericality: { other_than: 1 }
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :shipping_day_id, numericality: { other_than: 1 }
     validates :price
+  end
+
+  class Article < ApplicationRecord
+    extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :category
+    belongs_to :status
+    belongs_to :shipping_cost
+    belongs_to :prefecture
+    belongs_to :shipping_day
   end
 end
