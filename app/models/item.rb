17d2df2b4f0bb3d256f3cd86e713b8b0613b4e95
mikiyaaa <katocha.m0220thankyou@gmail.com>
2021-02-10 12:18:18 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,14 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  with_options presence: true do
+    validates :item_name
+    validates :description
+    validates :category_id
+    validates :status_id
+    validates :shipping_cost_id
+    validates :prefecture_id
+    validates :shipping_day_id
+    validates :price
+  end
 end
