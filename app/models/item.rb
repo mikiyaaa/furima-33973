8bc25b2ab4efd590fb169adc69a54ce49e67274d
mikiyaaa <katocha.m0220thankyou@gmail.com>
2021-02-11 10:54:17 +0900

    出品機能の単体テスト

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,8 +15,7 @@ class Item < ApplicationRecord
       validates :shipping_day_id
     end
 
-    validates :price, inclusion: { in:300..9999999 }
-    validates :price, format: { with: /\A[0-9]+\z/ }
+    validates :price, inclusion: { in:300..9999999 }, format: { with: /\A[0-9]+\z/ }
   end
 
   class Article < ApplicationRecord
