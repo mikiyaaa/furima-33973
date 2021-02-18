177673cdbbd88c1c5edda1ba4901d8f5d0af249a
mikiyaaa <katocha.m0220thankyou@gmail.com>
2021-02-19 01:15:14 +0900

    ログインと出品者本人かの有無での表示変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,18 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_ordrer_item, only: [:index, :create]
+  before_action :move_to_items_index
+
   def index
-    @item = Item.find(params[:item_id])
+    
     @order_destination = OrderDestination.new
   end
 
   def create 
-    @item = Item.find(params[:item_id])
     @order_destination = OrderDestination.new(order_params)
     if @order_destination.valid?
       @order_destination.save
+      redirect_to controller: :items, action: :index
     else
       render :index
     end
@@ -17,6 +21,16 @@ class OrdersController < ApplicationController
   private
   def order_params
     params.require(:order_destination).permit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_ordrer_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_items_index
+    if current_user == @item.user
+      redirect_to controller: :items, action: :index
+    end
   end
 
 end
