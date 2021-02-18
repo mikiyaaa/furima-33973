98f1b31656100ee5b9070efeac4e328cecd8c4a3
mikiyaaa <katocha.m0220thankyou@gmail.com>
2021-02-18 17:02:24 +0900

    ordersテーブルとdestinationsテーブルへの保存

--- app/models/order_destination.rb
+++ app/models/order_destination.rb
@@ -1,6 +1,6 @@
 class OrderDestination
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :city, :address, :building_number, :phone_number, :order_id, :user_id, :item_id
+  attr_accessor :post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :order_id, :user_id, :item_id
 
   with_options presence: true do
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
@@ -8,13 +8,15 @@ class OrderDestination
     validates :city
     validates :address
     validates :phone_number, format: {with: /\A\d{11}\z/}
-    validates :order_id
+    # validates :order_id
     validates :user_id
     validates :item_id
   end
 
   def save
-    order = Order.create(user_id: user_id, item: item_id)
-    Destination.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, building_number: building_number, phone_number: phone_number, order_id: order_id)
+    # 購入情報を保存し、変数orderに代入
+    order = Order.create(user_id: user_id, item_id: item_id)
+    # 配送先を保存
+    Destination.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
 end
